a230978b01fdfb28aa5553e8aa3a50cea7a261e3
Yuto <Yuto>
2023-07-10 23:51:47 +0900

    test

--- pj5_practice/src/gui/view.py
+++ pj5_practice/src/gui/view.py
@@ -1,26 +1,31 @@
 import tkinter as tk
 
 
-class View:
-    HEIGHT = 600
-    WIDTH = 800
+class UnderlineEntry(tk.Entry):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.config(borderwidth=0, relief="flat", insertwidth=1)
+        self.underline_color = "white"  # 下線の色を指定
 
+    def paint_underline(self):
+        x, y, width, height = self.bbox("insert")
+        self.create_line(x, y + height - 1, x + width, y + height - 1, fill=self.underline_color)
+
+    def on_configure(self, event=None):
+        self.delete("underline")
+        self.paint_underline()
+
+    def on_key_press(self, event=None):
+        self.after(1, self.on_configure)
+
+
+class View:
     def __init__(self, app, model):
         self.master = app
         self.model = model
-
-        self.monitor_height = app.winfo_screenheight()
-        print(self.monitor_height)
-        self.master.geometry(f"{View.WIDTH}x{View.HEIGHT}")
-        App_text = tk.Label(self.master, text="HELLO APP")
-        App_text.grid(column=0, row=0)
-        Frame_left = tk.Frame(self.master, height=200, width=400)
-        Frame_left.grid(column=0, row=1)
-        Frame_right = tk.Frame(self.master)
-        Frame_right.grid(column=1, row=1)
-
-        canvas1 = tk.Canvas(Frame_right, width=200, height=200, bg="green")
-        canvas1.grid(column=0, row=0)
-
-        button1 = tk.Button(Frame_left, text="test", height=20, width=30)
-        button1.grid(column=0, row=0)
+        self.frame_main = tk.Frame(self.master, width=300, height=200, bg="black")
+        self.frame_main.grid(column=0, row=0)
+        self.frame_main.rowconfigure(0, minsize=400)
+        entry = UnderlineEntry(self.frame_main, width=200)
+        entry.rowconfigure(0, minsize=400)
+        entry.pack()
